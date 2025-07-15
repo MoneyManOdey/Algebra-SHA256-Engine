1ad69eaf3e3b38bf14d64b5a2bbac45f983d263e
Oliver Odusanya <oliverodusanya@Olivers-MacBook-Pro.local>
2025-07-15 12:14:13 -0400

    🛠 UGhostMintWidget: implement UUserWidget overrides; add Slate/UMG deps

---
+++ Source/BitcoinSSD/Private/GhostMintWidget.cpp
@@ -0,0 +1,26 @@
+#include "GhostMintWidget.h"
+#include "Components/Button.h"
+#include "Components/Slider.h"
+#include "Components/DataTable.h"
+#include "Components/EditableTextBox.h"
+
+void UGhostMintWidget::NativePreConstruct()
+{
+    Super::NativePreConstruct();
+}
+
+void UGhostMintWidget::NativeConstruct()
+{
+    Super::NativeConstruct();
+    // TODO: bind widget events or initialize logic
+}
+
+void UGhostMintWidget::NativeDestruct()
+{
+    Super::NativeDestruct();
+}
+
+void UGhostMintWidget::NativeTick(const FGeometry& MyGeometry, float InDeltaTime)
+{
+    Super::NativeTick(MyGeometry, InDeltaTime);
+}
